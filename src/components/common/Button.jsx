88eb9dbda8509208bf5cfe680bcaea268f07190a
holdMyTea <oleksandr.pohrebniak42@gmail.com>
2020-06-14 13:46:54 -0400

    Add eslint, format initial files

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from 'react'
 
-const Button = ({onClick, label, classes, disabled}) => {
-    return (
-        <button
-        disabled={disabled}
-        onClick={onClick}
-        className={classes}
-        >
-            {label}
-        </button>
-    );
-};
+const Button = ({ onClick, label, classes, disabled }) => {
+  return (
+    <button
+      disabled={disabled}
+      onClick={onClick}
+      className={classes}
+    >
+      {label}
+    </button>
+  )
+}
 
-export default Button;
+export default Button
--- src/components/common/Table.jsx
+++ src/components/common/Table.jsx
@@ -1,28 +1,28 @@
-import React from 'react';
+import React from 'react'
 
-function Table({columns, data, tableDescriptor}) {
-    return (
-        <table className="table table-dark">
-            <thead>
-            <tr>
-                <th scope="col">{tableDescriptor}</th>
-                {columns.map(columnTitle => (
-                    <th key={columnTitle} scope="col">{columnTitle}</th>
-                ))}
-            </tr>
-            </thead>
-            <tbody>
-            {data.map((item, index) => (
-                <tr key={item.id}>
-                    <th scope="row">{++index}</th>
-                    {columns.map(columnTitle => (
-                        <td key={item[columnTitle]+columnTitle}>{item[columnTitle]}</td>
-                    ))}
-                </tr>
+function Table ({ columns, data, tableDescriptor }) {
+  return (
+    <table className="table table-dark">
+      <thead>
+        <tr>
+          <th scope="col">{tableDescriptor}</th>
+          {columns.map(columnTitle => (
+            <th key={columnTitle} scope="col">{columnTitle}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((item, index) => (
+          <tr key={item.id}>
+            <th scope="row">{++index}</th>
+            {columns.map(columnTitle => (
+              <td key={item[columnTitle] + columnTitle}>{item[columnTitle]}</td>
             ))}
-            </tbody>
-        </table>
-    )
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
 }
 
-export default Table;
+export default Table
